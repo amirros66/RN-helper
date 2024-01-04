5898a17d16382a073d1ea99899c7af0e4b98250d
amirros66 <143261344+amirros66@users.noreply.github.com>
2024-01-04 19:42:29 +0100

    Change styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import background from "./images/background.jpg";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ backgroundImage: `url(${background})` }}>
       <HomePage />
     </div>
   );
