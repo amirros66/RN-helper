86c98683a18b3b4569ddca01a365aea570d15dd3
amirros66 <143261344+amirros66@users.noreply.github.com>
2024-01-14 19:00:28 +0100

    Remove bmiCalculator from store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import bmiSliceReducer from "./bmiCalculator/bmiSlice";
 import calculatorSliceReducer from "./calculator/calculatorSlice";
 import toDoListSliceReducer from "./toDoList/toDoListSlice";
 
 const store = configureStore({
   reducer: {
-    bmiCalculator: bmiSliceReducer,
     calculator: calculatorSliceReducer,
     toDoList: toDoListSliceReducer,
   },
